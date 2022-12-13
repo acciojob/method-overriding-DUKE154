7847bf2a655d022e29a7e834a30e8caa3634d1c0
Dinesh Kamble <dineshkamble475@gmail.com>
2022-12-13 19:24:27 +0530

    pehla commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,32 @@
 package com.driver;
 
+
+
 public class Main {
-  
+
+   public static void main(String[] args) {
+
+        B b = new B();
+        b.meth();
+
+        B b1 = new B();
+        b1.meth();
+   }
+
+   
+   public class A {
+    
+    public String meth() {
+        return "Invoking method from class A";
+    }
+
+    @Override
+    public class B extends A{
+       
+        String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+
+    }
+}
 }
